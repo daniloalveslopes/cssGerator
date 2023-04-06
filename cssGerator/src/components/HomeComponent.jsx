3f6a8689796579b2795dd3983fb97ebea4609e22
daniloalveslopes <99082226+daniloalveslopes@users.noreply.github.com>
2023-04-06 13:50:02 -0300

    removi os butões que eu n tinha tirado o homeComponents

--- cssGerator/src/components/HomeComponent.jsx
+++ cssGerator/src/components/HomeComponent.jsx
@@ -7,10 +7,7 @@ const HomeComponent = () => {
             <h1 className="text-2xl font-medium mb-2">OPTIONS:</h1>
             <div className="container">
                 <Link to={"/button"} className="card border-2 border-black bg-blue-700 text-white rounded-lg p-3"><h1 className="text-center">BUTTON</h1></Link> 
-                <Link to={"/button"} className="card border-2 border-black bg-blue-700 text-white rounded-lg p-3"><h1 className="text-center">BUTTON</h1></Link> 
-                <Link to={"/button"} className="card border-2 border-black bg-blue-700 text-white rounded-lg p-3"><h1 className="text-center">BUTTON</h1></Link> 
-                <Link to={"/button"} className="card border-2 border-black bg-blue-700 text-white rounded-lg p-3"><h1 className="text-center">BUTTON</h1></Link> 
-                <Link to={"/button"} className="card border-2 border-black bg-blue-700 text-white rounded-lg p-3"><h1 className="text-center">BUTTON</h1></Link> 
+                 
            
            
            
