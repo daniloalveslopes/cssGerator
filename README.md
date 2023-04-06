aa4d74328e32eba1bd758fb9cc4fe2b6c05fe3f0
daniloalveslopes <99082226+daniloalveslopes@users.noreply.github.com>
2023-04-05 22:46:51 -0300

    criei o estilo base da pagina, precida de algumas alterções.

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-ola
+Olá me chamo Danilo, e estou criando este projeto para práticar o REACT, que eu estou atualmente estudando.
---
+++ cssGerator/src/components/Header.jsx
@@ -0,0 +1,7 @@
+export const Header = () => {
+    return (
+        <header className="p-5 bg-blue-700">
+            <h1 className="font-medium text-3xl max-sm:text-xl text-center text-white">CSS <span>GERATOR</span></h1>
+        </header>
+    )
+}
